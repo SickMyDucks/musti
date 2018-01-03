e9131d697d52170cf8374cdc5995252bb376c3e8
Neil Richter <me@neilrichter.com>
2018-01-03 18:45:20 +0100

    Upload writes in database and redirects

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
     require('init.php');
     $target_dir = "users/".$_SESSION['username']."/";
-    echo $target_dir."<br>";
-    $target_file = $target_dir . basename($_FILES["upload"]["name"]);
+    $file_name = basename($_FILES["upload"]["name"]);
+    $target_file = $target_dir . $file_name;
     $uploadOk = 1;
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
@@ -11,7 +11,11 @@
 
     if (move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        $q = "INSERT INTO `files` (`id`, `file_name`, `file_path`, `owned_by_user_id`, `modified_last`) VALUES (NULL, '".$file_name."', '".$target_file."', '".$_SESSION['id']."', '".date('Y-m-d H:i:s')."')";
+        mysqli_query($link, $q);
+        header( 'refresh: 2; url=/index.php' );
+        echo "<br>Redirection in 2 seconds.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "<br>Sorry, there was an error uploading your file.";
     }
 ?>
