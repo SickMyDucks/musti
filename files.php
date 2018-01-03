b7887390eb6da9e3def02a04a3cafc26ca88b977
Neil Richter <me@neilrichter.com>
2018-01-03 18:09:54 +0100

    Added form with input for files

--- files.php
+++ files.php
@@ -7,27 +7,21 @@
         $data[] = $row;
     }
 ?>
-    <table border="0">
-    <?php foreach ($data as $article):?>
-        <tr>
-            <td><i class="fa fa-file" aria-hidden="true"></i></td>
-            <td><?= $article["file_name"]?></td>
-            <td><?= $article["modified_last"]?></td>
-            <td><i class="fa fa-download" aria-hidden="true"></i></td>
-            <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-            <td><i class="fa fa-trash" aria-hidden="true"></i></td>
-        </tr>
-    <?php endforeach; 
-    //$files = ob_get_contents();
-    //ob_end_clean();
-    ?>
-    </table>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+    <label for="upload">Upload a a file : </label><input type="file" name="upload" id="upload"><br>
+    <input type="submit" value="Send">
+</form>
 
-<!-- <table>
+<table border="0">
+<?php foreach ($data as $article):?>
     <tr>
-        <td></td>
-        <td>Modified : </td>
+        <td><i class="fa fa-file" aria-hidden="true"></i></td>
+        <td><?= $article["file_name"]?></td>
+        <td><?= $article["modified_last"]?></td>
+        <td><i class="fa fa-download" aria-hidden="true"></i></td>
+        <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
+        <td><i class="fa fa-trash" aria-hidden="true"></i></td>
     </tr>
-</table> -->
-
+<?php endforeach;?>
+</table>
 
