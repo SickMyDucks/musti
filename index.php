65b6123b80f503fcfdeb8d1e92ac0fde60cbb8f5
Neil Richter <me@neilrichter.com>
2018-01-03 17:55:23 +0100

    Edited successful connection message

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ ob_start();
 if (!isset($_SESSION['username'])): ?>
 <h3>Login to upload files.</h3>
 <?php else: ?>
-<h3>Connected, you can now upload files.</h3><br>
 <?php include('files.php')?>
 <?php endif;
 $content = ob_get_contents();
