81e602a3bb3ee1e8adcbb1de08e2323e5a13ca90
Neil Richter <me@neilrichter.com>
2018-01-04 23:26:12 +0100

    Edited style

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ window.onload = function() {
             validate.innerHTML = '<button type="submit" name="' + buttonValue + '" form="files"><i class="fa fa-check" aria-hidden="true"></i></button>';
             cancel = document.createElement('span');
             cancel.innerHTML = '<button><i class="fa fa-times" aria-hidden="true"></i></button>';
-            this.parentElement.children[1].appendChild(validate);
-            this.parentElement.children[1].appendChild(cancel);
+            this.parentElement.children[1].appendChild(document.createElement('div'));
+            this.parentElement.children[1].querySelector('div').appendChild(validate);
+            this.parentElement.children[1].querySelector('div').appendChild(cancel);
             this.parentElement.children[1].focus();
             validate.onclick = function() {
-                this.children[0].value = this.parentElement.innerText;
+                this.children[0].value = this.parentElement.parentElement.innerText;
             };
             this.parentElement.children[1].onblur = function() {
                 this.parentElement.children[1].removeAttribute('contenteditable');
-                this.parentElement.children[1].removeChild(this.parentElement.children[1].children[0]);
-                this.parentElement.children[1].removeChild(this.parentElement.children[1].children[0]);
+                this.parentElement.children[1].removeChild(this.parentElement.children[1].querySelector('div'));
             };
         };
     }
