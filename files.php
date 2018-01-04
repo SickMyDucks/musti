b5e77d03df63f464c6f2be9a72faba8993134526
Neil Richter <me@neilrichter.com>
2018-01-04 19:34:29 +0100

    Fixed HTML

--- files.php
+++ files.php
@@ -1,5 +1,5 @@
 <?php 
-    $q = "SELECT file_name, modified_last FROM files WHERE owned_by_user_id = '". $_SESSION['id'] . "'";
+    $q = "SELECT id, file_name, modified_last FROM files WHERE owned_by_user_id = '". $_SESSION['id'] . "'";
     $result = mysqli_query($link, $q);
     $data = [];
     while ($row = mysqli_fetch_assoc($result))
@@ -11,22 +11,20 @@
     <label for="upload">Upload a a file : </label><input type="file" name="upload" id="upload" required><br>
     <input type="submit" value="Send";>
 </form>
-
-<table border="0">
-<?php if (count($data) == 0):?>
-    <tr><td>No file existing.</td></tr>
-
-<?php 
-    endif;
-    foreach ($data as $article):
-?>
-    <tr>
-        <td><i class="fa fa-file" aria-hidden="true"></i></td>
-        <td><?= $article["file_name"]?></td>
-        <td><?= $article["modified_last"]?></td>
-        <td><a href="http://musti.keepthis4.me/users/<?= $_SESSION['username'].'/'.$article["file_name"]?>" download="<?=$article["file_name"]?>"><i class="fa fa-download" aria-hidden="true"></i></a></td>
-        <td><i class="fa fa-pencil" aria-hidden="true"></i></td>
-        <td><i class="fa fa-trash" aria-hidden="true"></i></td>
-    </tr>
-<?php endforeach;?>
-</table>
+    <table border="0">
+    <?php if (count($data) == 0):?>
+        <tr><td>No file existing.</td></tr>
+    <?php endif;?>
+        <form action="actions.php" method="POST">
+        <?php foreach ($data as $article):?>
+            <tr>
+                <td><i class="fa fa-file" aria-hidden="true"></i></td>
+                <td><?= $article["file_name"]?></td>
+                <td><?= $article["modified_last"]?></td>
+                <td><button type="submit" name="download<?= $article["id"]?>"><i class="fa fa-download" aria-hidden="true"></i></button></td>
+                <td><button type="submit" name="edit<?= $article["id"]?>"><i class="fa fa-pencil" aria-hidden="true"></i></button></td>
+                <td><button type="submit" name="delete<?= $article["id"]?>"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+            </tr>
+        <?php endforeach;?>
+        </form>
+    </table>
