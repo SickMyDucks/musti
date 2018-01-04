e5a589e542d8d9ac910f46d3ba1fd43e6b4ff1a4
Neil Richter <me@neilrichter.com>
2018-01-04 21:52:38 +0100

    Edited form

--- files.php
+++ files.php
@@ -9,7 +9,7 @@
 ?>
 <form action="upload.php" method="POST" enctype="multipart/form-data">
     <label for="upload">Upload a a file : </label><input type="file" name="upload" id="upload" required><br>
-    <input type="submit" value="Send";>
+    <input type="submit" value="Send">
 </form>
     <table border="0">
     <?php if (count($data) == 0):?>
@@ -22,7 +22,7 @@
                 <td><?= $article["file_name"]?></td>
                 <td><?= $article["modified_last"]?></td>
                 <td><button type="submit" name="download<?= $article["id"]?>"><i class="fa fa-download" aria-hidden="true"></i></button></td>
-                <td><button type="submit" name="edit<?= $article["id"]?>"><i class="fa fa-pencil" aria-hidden="true"></i></button></td>
+                <td name="edit<?= $article["id"]?>"><i class="fa fa-pencil" aria-hidden="true"></i></td>
                 <td><button type="submit" name="delete<?= $article["id"]?>"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
             </tr>
         <?php endforeach;?>
