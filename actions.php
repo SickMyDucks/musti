6ec837e7f74b05eb2500f8a929f8ce38435bc332
Neil Richter <me@neilrichter.com>
2018-01-04 21:53:42 +0100

    Now renames in Database and directory according to user's input

--- actions.php
+++ actions.php
@@ -1,17 +1,19 @@
 <?php
 require('init.php');
 $index = array_keys($_POST)[0];
+var_dump($_POST);
+var_dump($index);
 if (isset($_POST)) {
     $file_id = filter_var($index, FILTER_SANITIZE_NUMBER_INT);
     if(substr($index, 0, 8) == "download") {
-
+        
     } else if (substr($index, 0, 4) == "edit") {
         $q = "SELECT file_name FROM files WHERE id = " . $file_id;
         $filename = mysqli_query($link, $q);
         $filename = mysqli_fetch_assoc($filename);
-        $q = "UPDATE files SET file_name = 'editedfile', file_path = 'users/" . $_SESSION['username'] . "/" . "editedfile'" . " WHERE id = " . $file_id;
+        $q = "UPDATE files SET file_name = '" . $_POST[$index] . "', file_path = 'users/" . $_SESSION['username'] . "/" . $_POST[$index] . "'" . " WHERE id = " . $file_id;
         mysqli_query($link, $q);
-        rename('users/'.$_SESSION['username'].'/'.$filename["file_name"], 'users/'.$_SESSION['username'].'/'. 'editedfile');
+        rename('users/'.$_SESSION['username'].'/'.$filename["file_name"], 'users/'.$_SESSION['username'].'/'. $_POST[$index]);
     } else {
         $q = "SELECT file_name FROM files WHERE id = " . $file_id;
         $filename = mysqli_query($link, $q);
