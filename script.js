42b852bdd0b5bfbdd0e0009da99d20205cde6c84
Neil Richter <me@neilrichter.com>
2018-01-06 14:39:32 +0100

    Edited targets

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.onload = function() {
         edit[i].onclick = function() {
             buttonValue = this.attributes[0].value;
             console.log(buttonValue);
-            this.parentElement.children[1].setAttribute('contenteditable', 'true');
-            this.parentElement.children[1].setAttribute('name', buttonValue);
+            this.parentElement.children[1].children[0].setAttribute('contenteditable', 'true');
+            this.parentElement.children[1].children[0].setAttribute('name', buttonValue);
             validate = document.createElement('span');
             validate.innerHTML = '<button type="submit" name="' + buttonValue + '" form="files"><i class="fa fa-check" aria-hidden="true"></i></button>';
             cancel = document.createElement('span');
@@ -13,9 +13,10 @@ window.onload = function() {
             this.parentElement.children[1].appendChild(document.createElement('div'));
             this.parentElement.children[1].querySelector('div').appendChild(validate);
             this.parentElement.children[1].querySelector('div').appendChild(cancel);
-            this.parentElement.children[1].focus();
+            this.parentElement.children[1].children[0].focus();
             validate.onclick = function() {
                 this.children[0].value = this.parentElement.parentElement.innerText;
+                data = this.children[0].value;
             };
             this.parentElement.children[1].onblur = function() {
                 this.parentElement.children[1].removeAttribute('contenteditable');
