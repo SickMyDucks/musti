3af5f762f7e41db581d3091ed563179c0d2a664c
Neil Richter <me@neilrichter.com>
2018-01-07 02:56:28 +0100

    Added init.php

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+/* EDIT THE CREDENTIALS */
+$host = 'host';
+$username = 'username';
+$password = 'password';
+$database = 'database';
+$link = mysqli_connect($host, $username, $password, $database);
+session_start();
